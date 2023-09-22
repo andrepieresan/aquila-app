f6985a0f78d7b4bd50d00acafbb4fc4d103b794b
Andre Pieresan <andre.pieresan@dealerup.com.br>
2023-09-22 20:44:22 -0300

    style modal os

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -7,60 +7,39 @@
 
       <q-separator />
 
-      <q-card-section class="scroll">
+      <q-card-section class="scroll modalContent">
         <q-form
           @submit="onSubmit"
           @reset="onReset"
-          style="height: 70vh; width: 60vw; padding: 1em"
+          style="height: 70vh; width: 60vw"
         >
           <div style="float: left; width: 40%">
-            <q-input
-              standout
-              v-model="name"
-              label="Nome"
-              lazy-rules
-              :rules="[(val) => (val && val.length > 0) || '*']"
-            />
-            <q-input
-              standout
-              v-model="phone"
-              label="Telefone"
-              lazy-rules
-              :rules="[(val) => (val && val.length > 0) || '*']"
-            />
+            <q-input standout v-model="name" label="Nome" />
+            <q-input standout v-model="phone" label="Telefone" lazy-rules />
             <q-input standout v-model="doc" label="Documento" />
             <q-input standout v-model="doc" label="Endereço" />
           </div>
           <div style="float: right; width: 60%">
-            <q-input
-              standout
-              v-model="product"
-              label="Modelo"
-              lazy-rules
-              :rules="[(val) => (val && val.length > 0) || '*']"
-            />
-            <q-input
-              standout
-              v-model="phone"
-              label="Marca"
-              lazy-rules
-              :rules="[(val) => (val && val.length > 0) || '*']"
-            />
-            <q-input
-              standout
-              v-model="phone"
-              label="Serial"
-              lazy-rules
-              :rules="[(val) => (val && val.length > 0) || '*']"
-            />
-
+            <q-input standout v-model="product" label="Modelo" />
+            <q-input standout v-model="phone" label="Marca" />
+            <q-input standout v-model="phone" label="Serial" />
+            <q-input standout v-model="text" label="#######" />
+          </div>
+          <div style="float: left; width: 100%; padding: 1em">
             <q-select
               standout
               v-model="model"
               :options="options"
               label="Situação da Ordem de Serviço"
             />
-            <q-input standout v-model="text" label="Observação" autogrow />
+          </div>
+          <div style="float: left; width: 100%; padding: 1em">
+            <q-input
+              standout
+              v-model="text"
+              type="textarea"
+              label="Observação"
+            />
           </div>
         </q-form>
       </q-card-section>
@@ -101,6 +80,6 @@ export default defineComponent({
 </script>
 <style lang="scss">
 .q-input {
-  padding: 15px;
+  padding: 1em;
 }
 </style>
